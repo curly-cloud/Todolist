76878ff685b83ec6d950ebc0cac9b5828b019aac
Siré Sonko <kahddy@gmail.com>
2021-11-29 19:28:13 +0100

    Date

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,12 @@
 const app = {
+
     init : function () {
-        console.log("le jeu peut commencer ");
+        //quand l'appli démarre on affiche la date 
+         display.date(); 
     }
+    
 }
+ /* console.log(GetCurrentDate); */ 
 //quand la page est lancé le jeux peut commencer 
 //ecouteur d'evenement sur la page 
 document.addEventListener('DOMContentLoaded', app.init);
---
+++ JS/date.js
@@ -0,0 +1,25 @@
+
+
+const display = {
+//Display date 
+/* addDateToDOM: function() {
+    
+   }, */
+     date : function(fullDate) {
+        //recupérer la date d'ajourd'hui
+        const currentDate =  new Date();
+        const options = { 
+        weekday: 'long', 
+        year: 'numeric',
+        month: 'long', 
+        day: 'numeric'
+        };
+        fullDate = currentDate.toLocaleDateString('fr-FR', options);
+       //Selection de l'id
+        let dateOfDay = document.querySelector('#date-full');
+        //insertion
+        dateOfDay.innerHTML = fullDate; 
+        return fullDate;
+    } 
+}
+
